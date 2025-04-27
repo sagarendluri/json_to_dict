f73b64b29dd5ab8a46a760550bab773468a5c2a3
Hema sagar Endluri <56343742+sagarendluri@users.noreply.github.com>
2025-04-27 17:12:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ from pydantic import BaseModel
 
 from fastapi.middleware.cors import CORSMiddleware
 
+
+origins = [
+    "https://sites.google.com/view/ai-research-hub/home",  # Replace this with your actual Google Sites URL
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allow all origins, or specify your Google Sites domain
+    allow_origins=origins,  # Allow all origins, or specify your Google Sites domain
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
